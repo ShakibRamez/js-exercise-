e731cfe6de77614494064d80dfd63ce1b3083216
ShakibRamez <shakibramiz1@icloud.com>
2025-06-01 01:50:54 +0430

    they are all of example and let way

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 // let and const varibale
 
 // 1. let 
-let age = 25;
-console.log(age);
+// let age = 25;
+// console.log(age);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
+
+// let channelName;
+// channelName = "shakib youtube";
+// console.log(channelName);
+
+
+// let myInformation = "Shakib";
+// let gender = "male";
+// let yearofBirth = 2004;
+// console.log(myInformation,gender,yearofBirth);
+
+
+// let myName = "ahmad";
+// myName = "shakib";
+// console.log(myName);
