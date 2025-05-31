0ee5e3b4260e0967e11f634463f527a5413e4f8c
ShakibRamez <shakibramiz1@icloud.com>
2025-06-01 03:05:06 +0430

    task

--- datatypes.js
+++ datatypes.js
@@ -1,39 +1,58 @@
-let num = 7;
-let newNum = 7.25;
+// let num = 7;
+// let newNum = 7.25;
 
 
-let favFood = "pizza";
-let favColor = "purple";
+// let favFood = "pizza";
+// let favColor = "purple";
 
-console.log(100 > 1);
-let js = "cool";
+// console.log(100 > 1);
+// let js = "cool";
 
-console.log(js == "cool"); 
+// console.log(js == "cool"); 
 
-let bigNumber = 29292929n;
-console.log(typeof bigNumber);
+// let bigNumber = 29292929n;
+// console.log(typeof bigNumber);
 
-let channelName;
-console.log(typeof channelName);
+// let channelName;
+// console.log(typeof channelName);
 
-let myList = ["shakib", true, "javascript", 20];
-console.log(myList[3]);
+// let myList = ["shakib", true, "javascript", 20];
+// console.log(myList[3]);
 
 
-let hello = function(){
-    console.log(hello);
+// let hello = function(){
+//     console.log(hello);
+// }
+
+// hello();
+// hello();
+// hello();
+
+// let character = {
+//     sname = "shahb",
+//     color = "purple",
+//     age = 20,
+//     young = true,
+//     }
+
+//     console.log(character.age);
+    
+
+// task
+
+let favDrink = "Coffee";
+
+let favNum = 7;
+
+function myFavs() {
+  console.log("Favorite Drink:", favDrink);
+  console.log("Favorite Number:", favNum);
 }
 
-hello();
-hello();
-hello();
+myFavs();
 
-let character = {
-    sname = "shahb",
-    color = "purple",
-    age = 20,
-    young = true,
-    }
 
-    console.log(character.age);
-    
+let person = {
+  name: "shakib",
+  age: 20
+};
