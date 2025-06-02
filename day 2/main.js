803170ab6fec1796b39e1c1f60db243e34585745
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 12:32:29 +0430

    if else statment

---
+++ day 2/main.js
@@ -0,0 +1,15 @@
+// if else statment
+
+// if(true){
+//     console.log("this is true");
+// }
+
+
+// if(1 < 10){
+//     console.log("1 is less than 10");
+// }
+
+
+// if(10 > 1){
+//     console.log("10 is greater than 1");
+// }
